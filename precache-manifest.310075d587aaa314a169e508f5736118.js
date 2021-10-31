07a399daf338db5d11cec47b005cd7cc7dbb2b4b
Stanislav Termosa <termosa.stanislav@gmail.com>
2021-10-31 11:35:01 +0200

    Updates

---
+++ precache-manifest.310075d587aaa314a169e508f5736118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40aec2ea886e39ab8c77a7bdfd4b0a9",
+    "url": "/use-patch/index.html"
+  },
+  {
+    "revision": "877a6db3667d2d9dfce4",
+    "url": "/use-patch/static/js/2.96b0ef20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-patch/static/js/2.96b0ef20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1219b3790e36b5595b8b",
+    "url": "/use-patch/static/js/main.88504266.chunk.js"
+  },
+  {
+    "revision": "7151b1d93faed830de27",
+    "url": "/use-patch/static/js/runtime-main.96cb5bc5.js"
+  }
+]);
